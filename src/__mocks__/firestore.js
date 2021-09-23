ad9cc2561cf3c2c0a273a2301c58f3356cb05b24
Mathieu Bouvet <mathieubouvet42@gmail.com>
2021-09-23 12:48:21 +0200

    Finish testing new bills ui

--- src/__mocks__/firestore.js
+++ src/__mocks__/firestore.js
@@ -1,33 +1,38 @@
+const mockedBills = {
+  get() {
+    return Promise.resolve({
+      docs: [
+        {
+          data() {
+            return {
+              id: "47qAXb6fIm2zOKkLzMro",
+              vat: "80",
+              fileUrl:
+                "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
+              status: "pending",
+              type: "Hôtel et logement",
+              commentary: "séminaire billed",
+              name: "Mocked bill",
+              fileName: "preview-facture-free-201801-pdf-1.jpg",
+              date: "2004-04-04",
+              amount: 4000,
+              commentAdmin: "ok",
+              email: "a@a",
+              pct: 20,
+            }
+          },
+        },
+      ],
+    })
+  },
+  add(bill) {
+    return Promise.resolve()
+  },
+}
+
 export default {
   bills() {
-    return {
-      get() {
-        return Promise.resolve({
-          docs: [
-            {
-              data() {
-                return {
-                  id: "47qAXb6fIm2zOKkLzMro",
-                  vat: "80",
-                  fileUrl:
-                    "https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a",
-                  status: "pending",
-                  type: "Hôtel et logement",
-                  commentary: "séminaire billed",
-                  name: "Mocked bill",
-                  fileName: "preview-facture-free-201801-pdf-1.jpg",
-                  date: "2004-04-04",
-                  amount: 4000,
-                  commentAdmin: "ok",
-                  email: "a@a",
-                  pct: 20,
-                }
-              },
-            },
-          ],
-        })
-      },
-    }
+    return mockedBills
   },
   storage: {
     ref() {
--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -1,8 +1,9 @@
-import { screen } from "@testing-library/dom"
+import { screen, fireEvent } from "@testing-library/dom"
 import NewBillUI from "../views/NewBillUI.js"
 import NewBill from "../containers/NewBill.js"
 import userEvent from "@testing-library/user-event"
 import mockedFirestore from "../__mocks__/firestore"
+import { ROUTES_PATH } from "../constants/routes.js"
 
 const spy = jest.spyOn(mockedFirestore.storage, "put")
 
@@ -54,6 +55,72 @@ describe("Given I am connected as an employee, on the new Bill page", () => {
       const fileInput = screen.getByTestId("file")
       userEvent.upload(fileInput, testFile)
       expect(spy).toHaveBeenCalledTimes(0)
+    })
+  })
+
+  describe("When I submit the creation form", () => {
+    test("Then I should be redirected to the bills page", () => {
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Employee", email: "a@a" })
+      )
+      const html = NewBillUI()
+      const onNavigate = jest.fn()
+      document.body.innerHTML = html
+      new NewBill({
+        document,
+        onNavigate,
+        firestore: null,
+        localStorage: window.localStorage,
+      })
+      const submitButton = screen.getByTestId("submit-new-bill")
+      userEvent.click(submitButton)
+
+      expect(onNavigate).toHaveBeenCalledWith(ROUTES_PATH["Bills"])
+    })
+
+    test("Then it should create the bill with the user inputed data", () => {
+      const spy = jest.spyOn(mockedFirestore.bills(), "add")
+      localStorage.setItem(
+        "user",
+        JSON.stringify({ type: "Employee", email: "a@a" })
+      )
+      const html = NewBillUI()
+      const onNavigate = jest.fn()
+      document.body.innerHTML = html
+      new NewBill({
+        document,
+        onNavigate,
+        firestore: mockedFirestore,
+        localStorage: window.localStorage,
+      })
+
+      const testData = {
+        expenseType: "IT et électronique",
+        expenseName: "test expanse name",
+        date: "2020-06-24",
+        amount: "156",
+        vat: "12",
+        pct: "42",
+        commentary: "test the commentary field"
+      }
+
+      userEvent.selectOptions(
+        screen.getByTestId("expense-type"),
+        testData.expenseType
+      )
+      userEvent.type(screen.getByTestId("expense-name"), testData.expenseName)
+      fireEvent.change(screen.getByTestId("datepicker"), {
+        target: { value: testData.date },
+      })
+      userEvent.type(screen.getByTestId("amount"), testData.amount)
+      userEvent.type(screen.getByTestId("vat"), testData.vat)
+      userEvent.type(screen.getByTestId("pct"), testData.pct)
+      userEvent.type(screen.getByTestId("commentary"), testData.commentary)
+
+      userEvent.click(screen.getByTestId("submit-new-bill"))
+
+      expect(spy).toHaveBeenCalledTimes(1)
     })
   })
 })
